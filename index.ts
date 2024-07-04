6d419c82a960398d393745f550726d379b5461c7
Rio D <riyo.s94@gmail.com>
2024-07-04 11:43:34 +0700

    fix node path and fs

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import path from 'path';
-import fs from 'fs';
+import path from 'node:path';
+import fs from 'node:fs';
 import { cons } from './src/constant';
 import { version, name } from './package.json'
 import { registerInstallationHook } from './src/hooks/dependencies';
